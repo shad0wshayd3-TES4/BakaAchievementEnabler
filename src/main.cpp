d1f263e6df7e12ed69d9bec0e63fb245fc142a66
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-04-23 09:28:23 -0600

    added patches for saves in progress

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,14 @@
 
 namespace REV
 {
-	void safe_fill(std::uintptr_t a_dst, std::uint8_t a_value, std::size_t a_count)
+	static void safe_fill(std::uintptr_t a_dst, std::uint8_t a_value, std::size_t a_count)
 	{
-		std::uint32_t old{ 0 };
-		bool success = REX::W32::VirtualProtect(
-			reinterpret_cast<void*>(a_dst),
-			a_count,
-			0x40,
-			std::addressof(old));
+		std::uint32_t protect{ 0 };
+		bool success = REX::W32::VirtualProtect(reinterpret_cast<void*>(a_dst), a_count, REX::W32::PAGE_EXECUTE_READWRITE, std::addressof(protect));
 		if (success)
 		{
 			std::fill_n(reinterpret_cast<std::uint8_t*>(a_dst), a_count, a_value);
-			success = REX::W32::VirtualProtect(
-				reinterpret_cast<void*>(a_dst),
-				a_count,
-				old,
-				std::addressof(old));
+			success = REX::W32::VirtualProtect(reinterpret_cast<void*>(a_dst), a_count, protect, std::addressof(protect));
 		}
 
 		assert(success);
@@ -28,14 +20,27 @@ namespace Hooks
 {
 	namespace
 	{
+		template <std::uintptr_t ID, std::ptrdiff_t OFF, std::size_t SIZE>
+		class hkNop
+		{
+		public:
+			static void Install()
+			{
+				static REL::Relocation target{ REL::Offset(ID) };
+				REV::safe_fill(target.address() + OFF, REL::NOP, SIZE);
+			}
+		};
+
 		void EnableAchievements()
 		{
-			static REL::Relocation target{ REL::Offset(0x499AE20) };
-			REV::safe_fill(target.address() + 0x00, REL::NOP, 0x09);
+			hkNop<0x49991C0, 0x09C, 0x0D>::Install();  // cmp rbx+2B1, r13b
+			hkNop<0x499AE20, 0x000, 0x09>::Install();  // mov rcx+2B0, 0x101
+			hkNop<0x499E550, 0x000, 0x10>::Install();  // mov rcx+2B1, r8b
 		}
 
 		void PreventBreakingAchievements()
 		{
+			// quicksave/autosave +0xED; movzx eax, rbx+2B1
 			static REL::Relocation target{ REL::Offset(0x48CC9E0) };
 			REV::safe_fill(target.address() + 0xED, 0x31, 0x01);
 			REV::safe_fill(target.address() + 0xEE, 0xC0, 0x01);
@@ -50,7 +55,7 @@ namespace Hooks
 		{
 			auto msg = std::vformat(
 				"""Baka Achievement Enabler has been DISABLED.\n"""
-				"""This version of the mod (1.0) was built for game version {}.\n"""
+				"""This version of the mod (1.1) was built for game version {}.\n"""
 				"""You are running a later version of the game: {}\n\n"""
 				"""You can continue playing, but achievements may not unlock!\n"""
 				"""Check the mod page for an update!""",
@@ -70,7 +75,6 @@ std::uint32_t DllMain([[maybe_unused]] REX::W32::HMODULE a_inst, std::uint32_t a
 	switch (a_reason)
 	{
 	case 1:  // DLL_PROCESS_ATTACH
-		// REX::W32::MessageBoxA(NULL, "Loaded", "Loaded", 0);
 		Hooks::Install();
 		break;
 	case 2:  // DLL_THREAD_ATTACH
